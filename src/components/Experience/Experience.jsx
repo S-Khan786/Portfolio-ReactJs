8e4823785e8d6569d05d11293ec327ef0074668c
S-Khan786 <osahilkhan654@gmail.com>
2025-07-01 17:43:55 +0530

    some changes on experience page

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -45,7 +45,7 @@ function Experience() {
 
             {/* Content Section */}
             <div
-              className={`w-full sm:max-w-md p-4 sm:p-8 rounded-2xl shadow-2xl border border-white bg-gray-900 backdrop-blur-md shadow-[0_0_20px_1px_rgba(130,69,236,0.3)] ${
+              className={`w-full sm:max-w-md p-4 sm:p-8 rounded-2xl border border-white bg-gray-900 backdrop-blur-md shadow-[0_0_20px_1px_rgba(130,69,236,0.3)] ${
                 index % 2 === 0 ? "sm:ml-0" : "sm:mr-0"
               } sm:ml-28 sm:mr-27 ml-8 transform transition-transform duration-300 hover:scale-105`}
             >
